193565b553ff6d54504be127a25c0a7e1795a75a
Neol <adrien.chre@gmail.com>
2014-08-26 10:36:26 +0200

    Clean fixers/Apostrophe.js

--- fixers/Apostrophe.js
+++ fixers/Apostrophe.js
@@ -1,15 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets */
 
-/*define(function (require, exports, module) {
-    "use strict";
-
-    function fix(content) {
-        return content.replace(/(\w+)'(\w)/gi, '$1’$2');
-    }
-    
-    exports.fix = fix;
-});*/
 define(function (require, exports, module) {
 	"use strict";
 	
