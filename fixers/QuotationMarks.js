e0e9ac3b9a1d32fcc946f68d9bd2442f0ec4b207
Neol <adrien.chre@gmail.com>
2014-08-30 14:07:15 +0200

    Move the locale feature on locale/fr

--- fixers/QuotationMarks.js
+++ fixers/QuotationMarks.js
@@ -4,31 +4,15 @@
 define(function (require, exports, module) {
     "use strict";
 
-    // Constants
-    var en_US = [{start: '“', end: '”'}, {start: '‘', end: '’'}],
-        // Unsafe characters are narrow no-break space in french quotes.
-        fr_FR = [{start: '« ', end: ' »'}, {start: '“', end: '”'}, {start: '‘', end: '’'}];
-
-    var regexp = /\B(["'])(.+?)(\1)\B/gm;
-
-    function _fixMarks(content, marksSets) {
+    function fix(content, locale) {
         var nestLevel = -1,
-            set = null;
+            regexp = /\B(["'])(.+?)(\1)\B/gm,
+            set = [{start: '“', end: '”'}, {start: '‘', end: '’'}];
 
         while (content.match(regexp) && marksSets.length > 0) {
             nestLevel++;
             set = marksSets[nestLevel];
             content = content.replace(regexp, set.start + '$2' + set.end);
-        }
-
-        return content;
-    }
-
-    function fix(content, locale) {
-        if (locale === 'fr_FR' || locale === 'fr') {
-            content = _fixMarks(content, fr_FR);
-        } else {
-            content = _fixMarks(content, en_US);
         }
 
         return content;
